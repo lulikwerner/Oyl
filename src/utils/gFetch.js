2efb3b9a48413c51bd94df4ec110f74c29aa230f
Lucila Werner <lulikwerner@gmail.com>
2023-06-29 20:49:33 -0400

    photo

--- src/utils/gFetch.js
+++ src/utils/gFetch.js
@@ -1,8 +1,8 @@
 let products = [
-  { id: 1, img: "../public/assets/img/dia.png" },
+  { id: 1, img: "/public/assets/img/dia.png" },
   { id: 2, img: "../public/assets/img/noche.png" },
-  { id: 3, img: "../public/assets/img/abrigos.png" },
-  { id: 4, img: "../public/assets/img/calzado.png" },
+  { id: 3, img: "../../public/assets/img/abrigos.png" },
+  { id: 4, img: "../assets/img/calzado.png" },
   { id: 5, img: "../public/assets/img/jeans.png" },
   { id: 6, img: "../public/assets/img/camisas.png" },
   { id: 7, img: "../public/assets/img/accesorios.png" },
